2d3fd41445aca8e9b5ea3239f852cf5564712c0d
ashikbillah699 <ashikbillah699@gmail.com>
2025-02-12 00:50:37 +0600

    added Feedback functionality and loaded all feedback data

--- src/page/deshboard/student/MyEnrollClassDetails.jsx
+++ src/page/deshboard/student/MyEnrollClassDetails.jsx
@@ -6,13 +6,20 @@ import useAxiosSecure from "../../../hooks/useAxiosSecure";
 import { useQuery } from "@tanstack/react-query";
 import { FaRegClock, FaUpload } from "react-icons/fa";
 import toast from "react-hot-toast";
-// import { useState } from "react";
+import { Rating } from "@smastrom/react-rating";
+import { useState } from "react";
+import { useContext } from "react";
+import { AuthContext } from "../../../provider/AuthProvider";
+import useAllClass from "../../../hooks/useAllClass";
 
 
 const MyEnrollClassDetails = () => {
     const { id } = useParams();
     const axiosSecure = useAxiosSecure();
-    // const [submited, setSubmited] = useState(false);
+    const [rating, setRating] = useState(0);
+    const { user } = useContext(AuthContext);
+    const [allClass] = useAllClass()
+    const ClassTitle = allClass.find(singleClass => singleClass._id === id)?.title || 'Class Not Found';
 
     const { data: myClassesDetails = [], refetch, isLoading } = useQuery({
         queryKey: ['myClassesDetails', id],
@@ -31,7 +38,6 @@ const MyEnrollClassDetails = () => {
             console.log(data)
             if (data.matchedCount > 0) {
                 toast.success('Successfully submited your assignment!!')
-                // setSubmited(true);
             }
         }
         catch (err) {
@@ -40,12 +46,93 @@ const MyEnrollClassDetails = () => {
         }
     }
 
+    const handleFeedback = async(e) => {
+        e.preventDefault();
+        const name = user?.displayName;
+        const photoURL = user?.photoURL;
+        const classTilte = ClassTitle;
+        const description = e.target.description.value;
+        const rate = rating;
+
+        const feedbackData = {name, photoURL, classTilte, description, rate};
+        console.table(feedbackData);
+
+        try{
+            const {data} = await axiosSecure.post('/feedback', feedbackData);
+            if(data.insertedId){
+                toast.success('Posted your feedback!!')
+                document.getElementById("my_modal_1").close();
+            }
+        }
+        catch(err){
+            console.log(err.message);
+            toast.error(err.message);
+        }
+    
+    }
+
     return (
         <div>
             <Helmet>
                 <title>SkillFlow  |  Class Details</title>
             </Helmet>
             <DeshboardBanner img={myEnrollClassDetailsBg} title={'My Enroll Class Details'}></DeshboardBanner>
+            {/* feedback */}
+            {/* <button className="btn btn-outline btn-info btn-sm m-6 w-40">TER </button> */}
+            <button className="btn btn-outline btn-info btn-sm m-6 w-40" onClick={() => document.getElementById('my_modal_1').showModal()}>TER</button>
+            <dialog id="my_modal_1" className="modal">
+                <form onSubmit={handleFeedback} className="modal-box">
+                    <div className="space-y-4">
+                        {/* Name */}
+                        <div>
+                            <label className="block font-medium text-gray-700">Name</label>
+                            <input type="text" disabled value={user?.displayName} placeholder="Enter your name" className="input input-bordered w-full" />
+                        </div>
+
+                        {/* Image */}
+                        <div>
+                            <label className="block font-medium text-gray-700">Image URL</label>
+                            <input type="text" disabled value={user?.photoURL} placeholder="Enter image URL" className="input input-bordered w-full" />
+                        </div>
+
+                        {/* Title */}
+                        <div>
+                            <label className="block font-medium text-gray-700">Class Title</label>
+                            <input type="text" disabled value={ClassTitle} placeholder="Enter class title" className="input input-bordered w-full" />
+                        </div>
+
+                        {/* Description */}
+                        <div>
+                            <label className="block font-medium text-gray-700">Feedback</label>
+                            <textarea
+                                name="description"
+                                placeholder="Write your feedback"
+                                className="textarea textarea-bordered w-full"
+                            ></textarea>
+                        </div>
+
+                        {/* Rating */}
+                        <div>
+                            <label className="block font-medium text-gray-700">Rating</label>
+                            <div className="flex items-center gap-2">
+                                <Rating value={rating} onChange={setRating} className="text-yellow-500" />
+                                <span className="text-gray-600">{rating} Stars</span>
+                            </div>
+                        </div>
+
+                        {/* Submit Button */}
+                        <button className="btn btn-primary btn-sm w-full">Send Feedback</button>
+                    </div>
+                    <div className="modal-action">
+                        <form method="dialog">
+                            {/* if there is a button in form, it will close the modal */}
+                            <button className="btn">Close</button>
+                        </form>
+                    </div>
+                </form>
+            </dialog>
+
+            {/* all assignment */}
             <div className="overflow-x-auto p-6">
                 <table className="table w-full bg-white shadow-lg rounded-xl">
                     <thead className="bg-blue-500 text-white">
@@ -74,7 +161,7 @@ const MyEnrollClassDetails = () => {
                                     />
                                     <button
                                         // disabled={submited}
-                                        onClick={() => handleSubmit(assignment.classId)}
+                                        onClick={() => handleSubmit(assignment.classId, assignment._id)}
                                         className="flex items-center justify-center gap-2 bg-blue-500 hover:bg-blue-600 text-white py-1 px-3 rounded-md disabled:bg-gray-400 disabled:cursor-not-allowed">
                                         <FaUpload /> Submit
                                     </button>
--- src/page/homePage/Feedback.jsx
+++ src/page/homePage/Feedback.jsx
@@ -6,47 +6,61 @@ import { Navigation } from 'swiper/modules';
 import '@smastrom/react-rating/style.css'
 import { Rating } from '@smastrom/react-rating';
 import CommonHead from '../../commonSection/CommonHead'
+import { useQuery } from '@tanstack/react-query';
+import useAxiosPublic from '../../hooks/useAxiosPublic';
 
 const Feedback = () => {
-    const reviews = [
-        {
-            _id: "1",
-            rating: 4.5,
-            details: "Their service was excellent and the team was extremely professional. Highly recommended!",
-            name: "John Doe"
-        },
-        {
-            _id: "2",
-            rating: 5,
-            details: "I’m really satisfied with the product quality. It exceeded my expectations!",
-            name: "Jane Smith"
-        },
-        {
-            _id: "3",
-            rating: 4,
-            details: "Good experience overall, but there’s still room for improvement in customer support.",
-            name: "Alex Johnson"
-        },
-    ]
+    const axiosPublic = useAxiosPublic();
+    // const reviews = [
+    //     {
+    //         _id: "1",
+    //         rating: 4.5,
+    //         details: "Their service was excellent and the team was extremely professional. Highly recommended!",
+    //         name: "John Doe"
+    //     },
+    //     {
+    //         _id: "2",
+    //         rating: 5,
+    //         details: "I’m really satisfied with the product quality. It exceeded my expectations!",
+    //         name: "Jane Smith"
+    //     },
+    //     {
+    //         _id: "3",
+    //         rating: 4,
+    //         details: "Good experience overall, but there’s still room for improvement in customer support.",
+    //         name: "Alex Johnson"
+    //     },
+    // ]
+
+    const {data: feedbacks= []} = useQuery({
+        queryKey: ['feddbacks'],
+        queryFn: async () =>{
+            const res = await axiosPublic.get('/feedback');
+            console.log(res.data);
+            return res.data
+        }
+    })
+    console.log(feedbacks);
+
     return (
         <div className='mb-20 mt-10'>
             <CommonHead heading={'FEEDBACK'}></CommonHead>
             <Swiper navigation={true} modules={[Navigation]} className="max-w-screen-xl mx-auto">
 
                 {
-                    reviews.map(review => <SwiperSlide
+                    feedbacks.map(review => <SwiperSlide
                         key={review._id}
                         className='text-center px-40 space-y-4'
                     >
                         <Rating
                             style={{ maxWidth: 150 }}
-                            value={review.rating}
+                            value={review.rate}
                             readOnly
                             className='mx-auto'
                         />
-                        <img className='mx-auto h-16 w-16' src="https://img.icons8.com/?size=50&id=38968&format=png" alt="" />
-                        <p className='text-sm'>{review.details}</p>
+                        <img className='mx-auto h-20 w-20 rounded-full' src={review.photoURL} alt="" />
                         <h3 className="text-xl uppercase font-mediam text-[#F36B27]">{review.name}</h3>
+                        <p className='text-sm'>{review.description}</p>
 
                     </SwiperSlide>)
                 }
